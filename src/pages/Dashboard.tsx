37b21f5b3db5f2148d329cff6f539ba5ad12aaa0
marty0228 <dhlee0228@naver.com>
2025-08-20 12:33:12 +0900

    ㅇㅇ을 추가함

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,8 +1,8 @@
 export default function Dashboard() {
   return (
     <section>
-      <h2>대시보드</h2>
-      <p>이번 주 진행 상황, 오늘의 퀘스트, 습관 리포트 등을 요약해서 보여줄 예정입니다.</p>
+      <h2>대시보드</h2>다
+      <p>이번 주 진행 상황, 오늘의 퀘스트, 습관 리포트 등을 요약해서 보여줄 예정입니다ㅇㅇ.</p>
     </section>
   );
 }
