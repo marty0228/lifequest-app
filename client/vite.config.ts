72127c1aff6e53d5d7c7bda287d658c55319666a
marty0228 <dhlee0228@naver.com>
2025-08-21 04:18:47 +0900

    프론트가 백엔드 API로 요청을 보낼 때 CORS 문제 없이 프록시로 연결

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,   // 프론트 개발 서버 포트
+    proxy: {
+      '/auth': 'http://localhost:4000',
+      '/quests': 'http://localhost:4000',
+      '/goals': 'http://localhost:4000',
+      '/schedules': 'http://localhost:4000',
+      '/me': 'http://localhost:4000',
+    }
+  }
 })
