1dcb94db1d497bc3a9f762e4b482e0a5ce305138
marty0228 <dhlee0228@naver.com>
2025-08-28 00:00:01 +0900

    로그인 직후 프로필 초기화

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import { supabase } from "./utils/supabase";
+import { ensureMyProfile } from "./utils/profileDb";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -11,3 +13,14 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     </BrowserRouter>
   </React.StrictMode>
 );
+
+supabase.auth.onAuthStateChange(async (event, session) => {
+  if (event === "SIGNED_IN" && session?.user) {
+    const meta = session.user.user_metadata || {};
+    await ensureMyProfile(session.user.id, {
+      username: meta.user_name ?? null,
+      displayName: meta.full_name ?? meta.name ?? null,
+      avatarUrl: meta.avatar_url ?? null,
+    });
+  }
+});
