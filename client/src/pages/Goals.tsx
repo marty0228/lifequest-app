4c874e34ea43f1fd59db8b53d9f2cd3a1777accd
marty0228 <dhlee0228@naver.com>
2025-09-01 21:53:09 +0900

    수정

--- client/src/pages/Goals.tsx
+++ client/src/pages/Goals.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect,  useState } from "react";
 import { supabase } from "../utils/supabase";
 import {
   listMyGoals, addGoal, updateGoal, removeGoal,
